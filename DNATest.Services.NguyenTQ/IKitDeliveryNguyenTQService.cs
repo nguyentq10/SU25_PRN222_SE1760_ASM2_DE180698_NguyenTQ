ea927cc7d95a03172e91912099b397915863d738
Trần Quang Nguyên <quangnguyentran2004@gmail.com>
2025-06-23 11:32:19 +0700

    Update Search and Pagging

--- DNATest.Services.NguyenTQ/IKitDeliveryNguyenTQService.cs
+++ DNATest.Services.NguyenTQ/IKitDeliveryNguyenTQService.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-using DNATest.Repositories.NguyenTQ.Models;
 namespace DNATest.Services.NguyenTQ
 {
     public interface IKitDeliveryNguyenTQService
@@ -13,7 +11,7 @@ namespace DNATest.Services.NguyenTQ
         
         Task<KitDeliveryNguyenTq> GetIdAsync(int id);
 
-        Task<List<KitDeliveryNguyenTq>> SearchAsync(int kitdeliveryId, string courierOut, int rating);
+        Task<List<KitDeliveryNguyenTq>> SearchAsync(string kitdeliveryId, string courierOut, decimal? rating);
         Task<int> CreateAsync(KitDeliveryNguyenTq kitdelivery);
         Task<int> UpdateAsync(KitDeliveryNguyenTq kitdelivery);
         Task<bool> DeleteAsync(int id);
